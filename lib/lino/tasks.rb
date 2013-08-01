be9c6facb9aa4cd37c381efa8c46bda5d3d843eb
Avdi Grimm <avdi@avdi.org>
2013-08-01 12:15:44 -0400

    Fix missing dependency.

--- lib/lino/tasks.rb
+++ lib/lino/tasks.rb
@@ -27,7 +27,7 @@ directory build_dir
 directory export_dir => [build_dir]
 
 export_files.each do |export_file|
-  file export_file => [export_dir] do |t|
+  file export_file => [export_dir, source_for_export_file(export_file)] do |t|
     source_file = source_for_export_file(export_file)
     mkdir_p export_file.pathmap("%d")
     sh *export_command_for(source_file, export_file)
